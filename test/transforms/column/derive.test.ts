d1d4eaffbdc6c2324c546867325d5f8c626a27a5
wmakeev <w.makeev@gmail.com>
2026-03-16 16:24:57 +0500

    refactor(tests)

--- test/transforms/column/derive.test.ts
+++ test/transforms/column/derive.test.ts
@@ -3,7 +3,7 @@ import {
   // @ts-expect-error no typings for compose
   compose
 } from 'node:stream'
-import test from 'node:test'
+import { test, suite } from 'node:test'
 import {
   createTableTransformer,
   FlattenTransform,
@@ -11,20 +11,21 @@ import {
 } from '../../../src/index.js'
 import { createTestContext } from '../../_common/TestContext.js'
 
-test('Column:Derive', async () => {
-  const tableTransformer = createTableTransformer({
-    context: createTestContext(),
-    transforms: [
-      tf.column.derive({
-        column: 'sum',
-        expression: `'a' + 'b'`
-      })
-    ]
-  })
+suite('Column:Derive', () => {
+  test('simple', async () => {
+    const tableTransformer = createTableTransformer({
+      context: createTestContext(),
+      transforms: [
+        tf.column.derive({
+          column: 'sum',
+          expression: `'a' + 'b'`
+        })
+      ]
+    })
 
-  const table =
-    /* prettier-ignore */
-    [
+    const table =
+      /* prettier-ignore */
+      [
       [
         ['a', 'b'],
         [1, 4]
@@ -35,19 +36,49 @@ test('Column:Derive', async () => {
       ]
     ]
 
-  const transformedRows: string[][] = await compose(
-    () => tableTransformer(table),
-    new FlattenTransform()
-  ).toArray()
+    const transformedRows: string[][] = await compose(
+      () => tableTransformer(table),
+      new FlattenTransform()
+    ).toArray()
 
-  assert.deepEqual(
-    transformedRows,
-    /* prettier-ignore */
-    [
+    assert.deepEqual(
+      transformedRows,
+      /* prettier-ignore */
+      [
       ['a', 'b', 'sum'],
       [ 1 ,  4 ,    5 ],
       [ 2 ,  5 ,    7 ],
       [ 3 ,  6 ,    9 ]
     ]
-  )
+    )
+  })
+
+  test('default value()', async () => {
+    const tableTransformer = createTableTransformer({
+      context: createTestContext(),
+      transforms: [
+        tf.column.derive({
+          column: 'val',
+          expression: `value()`
+        })
+      ]
+    })
+
+    const table = [
+      [['a'], [1]],
+      [[2], [3]]
+    ]
+
+    const transformedRows: string[][] = await compose(
+      () => tableTransformer(table),
+      new FlattenTransform()
+    ).toArray()
+
+    assert.deepEqual(transformedRows, [
+      ['a', 'val'],
+      [1, null],
+      [2, null],
+      [3, null]
+    ])
+  })
 })
